1b0fd0c0be07a506df78f3454c7f12ab26ba70eb
Vyom-Patel <78941921+Vyom-Patel@users.noreply.github.com>
2021-02-11 22:44:57 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,66 +25,21 @@ public class Main {
             tempList[i] = input.nextInt();
         }
 
-        // Averages temperature - @@@@@@@ ASK WHY IT THERE ARE SO MANY DECIMALS ON THE SIDE WHEN AVERAGE ALL 1's
+        // Averages temperature 
         double calcAvg = calcAvg(tempList);
         System.out.printf("The average temperature of the week is: %.2f degree %n", calcAvg);
-
-        // Display hottest temperature
-        /*    int calcMax = calcMax(tempList);
-        System.out.println("The highest temperature of the week is: " + calcMax + " degree");
-         */
- /*  // Display coldest temperature
-        int minTemp = minTemp(tempList);
-        System.out.println("The coldest temperature of the week is: " + minTemp + " degree");
-
-        int[] maxTempList = searchTemp(tempList, maxTemp);
-
-        for (int i = 0; i < maxTempList.length; i++) {
-            System.out.print("The hottest days of the week are: " + maxTempList[i]);
-
-            System.out.print(weekDay(tempList, maxTemp));
-        }
-    } */
     }
+    
 // Average the temperature
-
     public static double calcAvg(int[] array) {
         int tempTotal = array[0];
 
         // Total temperature values
         for (int i = 0; i < array.length; i++) {
-
             tempTotal = array[i] + tempTotal;
-
         }
-
         // Return temperature average.
         return ((double) tempTotal / array.length);
     }
-    /*
-// Get hottest temperature
-    public static int calcMax(int[] array) {
-        int max = array[0];
-
-        // Check and replace max temp
-        for (int i = 1; i < array.length; i++) {
-            if (max < array[i]) {
-                max = array[i];
-            }
-
-        }
-        return max;
-    }
-/*
-// Get coldest temperature
-    public static int minTemp(int[] array) {
-        int min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (min > array[i]) {
-                min = array[i];
-            }
-        }
-        return min;
-    }
-     */
+   
 }
